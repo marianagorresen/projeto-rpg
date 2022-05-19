ea22da994cf29ee23af77b1530602afbb1458c9a
Mariana Görresen <mariana@gorresen.com>
2022-05-19 10:13:59 -0300

    Update código.c

--- código.c
+++ código.c
@@ -26,8 +26,78 @@ typedef struct personagem
 
 PERSONAGEM personagem;
 
+void teste(PERSONAGEM P);
+
+int arma_1();
 
 void criar_personagem();
+
+void pontos_vida();
+
+int main()
+{
+	int opc, voltar = 1;
+	int pontos;
+	
+	while(voltar == 1){
+		printf("-------------------\n------- MENU ------\n-------------------\n");
+		printf("Digite opcao 1, 2 ou 3:\n");
+		printf("1- Comecar o jogo.\n2- Contar a historia.\n3- Fechar o jogo.\n");
+		scanf("%d", &opc);
+		
+		switch(opc){
+			case 1:
+				system("cls");
+				printf("Digite o nome do seu personagem: ");
+				fflush(stdin);
+				gets(personagem.nome);
+				
+				do
+				{
+				printf("Voce tem 15 pontos para distribuir entre os atributos:\nFORCA\tCONSTITUICAO\tAGILIDADE\tDESTREZA\n\n");
+				
+					printf("FORCA: ");
+					scanf("%d", &personagem.atributo.forca);
+					printf("CONSTITUICAO: ");
+					scanf("%d", &personagem.atributo.constituicao);
+					printf("AGILIDADE: ");
+					scanf("%d", &personagem.atributo.agilidade);
+					printf("DESTREZA: ");
+					scanf("%d", &personagem.atributo.destreza);
+					pontos = personagem.atributo.agilidade + personagem.atributo.constituicao + personagem.atributo.destreza + personagem.atributo.forca;
+					if(pontos > 15 || pontos < 15)
+					{
+						printf("Distribua os pontos dos atributos novamente.\n\n");
+						system("pause");
+						system("cls");
+					}
+				}
+				while(pontos != 15);
+				personagem.pv = 0;
+				
+				pontos_vida(&personagem.atributo.constituicao, &personagem.pv);
+				printf("Pontos de vida: %.d", personagem.pv);
+				
+				break;
+				
+			case 2:
+				
+				break;
+				
+			case 3:
+				printf("Fechando...");
+				break;
+				
+			default:
+				printf("valor invalido,tente novamente.\n\n");
+				system("pause");
+				system("cls");	
+				voltar = 1;
+		}
+			
+	}
+	
+}
 
 void pontos_vida(int* consta, int* pv)
 {
@@ -39,56 +109,16 @@ void pontos_vida(int* consta, int* pv)
 	
 }
 
-int main()
+void teste(PERSONAGEM P)
 {
-	int opc;
-	int pontos;
-	printf("-------------------\n------- MENU ------\n-------------------\n");
-	printf("Digite opcao 1, 2 ou 3:\n");
-	printf("1- Comecar o jogo.\n2- Contar a historia.\n3- Fechar o jogo.\n");
-	scanf("%d", &opc);
-	switch(opc){
-		case 1:
-			system("cls");
-			printf("Digite o nome do seu personagem: ");
-			fflush(stdin);
-			gets(personagem.nome);
-			
-			do
-			{
-			printf("Voce tem 15 pontos para distribuir entre os atributos:\nFORCA\tCONSTITUICAO\tAGILIDADE\tDESTREZA\n\n");
-			
-				printf("FORCA: ");
-				scanf("%d", &personagem.atributo.forca);
-				printf("CONSTITUICAO: ");
-				scanf("%d", &personagem.atributo.constituicao);
-				printf("AGILIDADE: ");
-				scanf("%d", &personagem.atributo.agilidade);
-				printf("DESTREZA: ");
-				scanf("%d", &personagem.atributo.destreza);
-				pontos = personagem.atributo.agilidade + personagem.atributo.constituicao + personagem.atributo.destreza + personagem.atributo.forca;
-				if(pontos > 15)
-				{
-					printf("Voce passou o limite de 15 pontos. Distribua os pontos dos atributos novamente.\n\n");
-					system("pause");
-					system("cls");
-				}
-			}
-			while(pontos != 15);
-			personagem.pv = 0;
-			pontos_vida(&personagem.atributo.constituicao, &personagem.pv);
-			printf("Pontos de vida: %.d", personagem.pv);
-			
-			break;
-			
-		case 2:
-			
-			break;
-			
-		case 3:
-			printf("Fechando...");
-			break;
-			
-	}
+	printf("dados atuais de jogador: \n");
+	
+	printf("nome :%s\n",P.nome);
+	printf("vida :%i\n",P.pv);
+	printf("forca %i\n",P.atributo.forca);
+	printf("destreza %i\n",P.atributo.destreza);
+	printf("constituicao %i\n",P.atributo.constituicao);
+	printf("agilidade %i\n",P.atributo.agilidade);
+	
 	
 }
