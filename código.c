de715cc7efcb7c3a862a79cef96d462c51009c94
Mariana Görresen <mariana@gorresen.com>
2022-05-19 19:12:03 -0300

    Update código.c

--- código.c
+++ código.c
@@ -44,8 +44,6 @@ void teste(PERSONAGEM P);
 
 void criar_personagem();
 
-void escolher_arma();
-
 void pontos_vida();
 
 int main()
@@ -54,104 +52,104 @@ int main()
 	int pontos;
 	int escolha_primeira_arma, escolha_armadura;
 
-		printf("-------------------\n------- MENU ------\n-------------------\n");
-		printf("Digite opcao 1, 2 ou 3:\n");
-		printf("1- Comecar o jogo.\n2- Contar a historia.\n3- Fechar o jogo.\n");
-		scanf("%d", &opc);
-		
-		switch(opc){
-			case 1:
-				system("cls");
-				printf("Digite o nome do seu personagem: ");
-				fflush(stdin);
-				gets(personagem.nome);
-				
-				do
-				{
-				printf("Voce tem 15 pontos para distribuir entre os atributos:\nFORCA\tCONSTITUICAO\tAGILIDADE\tDESTREZA\n\n");
-				
-					printf("FORCA: ");
-					scanf("%d", &personagem.atributo.forca);
-					printf("CONSTITUICAO: ");
-					scanf("%d", &personagem.atributo.constituicao);
-					printf("AGILIDADE: ");
-					scanf("%d", &personagem.atributo.agilidade);
-					printf("DESTREZA: ");
-					scanf("%d", &personagem.atributo.destreza);
-					pontos = personagem.atributo.agilidade + personagem.atributo.constituicao + personagem.atributo.destreza + personagem.atributo.forca;
-					if(pontos > 15 || pontos < 15)
-					{
-						printf("Distribua os pontos dos atributos novamente.\n\n");
-						system("pause");
-						system("cls");
-					}
-				}
-				while(pontos != 15);
-				personagem.pv = 0;
-			
-				pontos_vida(&personagem.atributo.constituicao, &personagem.pv);
-				printf("Pontos de vida: %.d\n", personagem.pv);
+	printf("-------------------\n------- MENU ------\n-------------------\n");
+	printf("Digite opcao 1, 2 ou 3:\n");
+	printf("1- Comecar o jogo.\n2- Contar a historia.\n3- Fechar o jogo.\n");
+	scanf("%d", &opc);
 
-				printf("\na hr de escolher a sua arma e comecar a sua jornada chegou...\n");
-				printf("vc tem tres opcoes de armas: \n1.katana : arma leve\n2.espada : arma leve\n3.machado : arma pesada\n\ndigite respectivamente 1,2 ou 3: ");
-				scanf("%i",&escolha_primeira_arma);
+	switch(opc){
+		case 1:
+			system("cls");
+			printf("Digite o nome do seu personagem: ");
+			fflush(stdin);
+			gets(personagem.nome);
 
-				if(escolha_primeira_arma == 1)
+			do
+			{
+			printf("Voce tem 15 pontos para distribuir entre os atributos:\nFORCA\tCONSTITUICAO\tAGILIDADE\tDESTREZA\n\n");
+
+				printf("FORCA: ");
+				scanf("%d", &personagem.atributo.forca);
+				printf("CONSTITUICAO: ");
+				scanf("%d", &personagem.atributo.constituicao);
+				printf("AGILIDADE: ");
+				scanf("%d", &personagem.atributo.agilidade);
+				printf("DESTREZA: ");
+				scanf("%d", &personagem.atributo.destreza);
+				pontos = personagem.atributo.agilidade + personagem.atributo.constituicao + personagem.atributo.destreza + personagem.atributo.forca;
+				if(pontos > 15 || pontos < 15)
 				{
-					strcpy(personagem.equipamento.arma.nome, "katana");
-					personagem.equipamento.arma.categoria = 1;
-					personagem.equipamento.arma = arma;
-				}
-				else if(escolha_primeira_arma == 2)
-				{
-					strcpy(personagem.equipamento.arma.nome, "espada");
-					personagem.equipamento.arma.categoria = 1;
-					personagem.equipamento.arma = arma;
-				}
-				else if(escolha_primeira_arma == 3)
-				{
-					strcpy(personagem.equipamento.arma.nome, "machado");
-					personagem.equipamento.arma.categoria = 2;
-					personagem.equipamento.arma = arma;
-				}
-				else
-				{
-					printf("resposta invalida,tente novamente.\n\n");
+					printf("Distribua os pontos dos atributos novamente.\n\n");
 					system("pause");
 					system("cls");
 				}
+			}
+			while(pontos != 15);
+			personagem.pv = 0;
 
-				printf("\nMomento de escolher a armadura\n");
-				printf("Escolha uma das 3 opcoes:\n1- armadura1\n2- armadura2\n3- armadura 3\n\ndigite respectivamente 1,2 ou 3: ");
-				scanf("%d", &escolha_armadura);
+			pontos_vida(&personagem.atributo.constituicao, &personagem.pv);
+			printf("Pontos de vida: %.d\n", personagem.pv);
 
-				if(escolha_armadura == 1){
-					strcpy(personagem.equipamento.armadura.nome, "armadura1");
-					personagem.equipamento.armadura.defesa = 5 + 1.5*personagem.atributo.constituicao;
-				}else if(escolha_armadura == 2){
-					strcpy(personagem.equipamento.armadura.nome, "armadura2");
-					personagem.equipamento.armadura.defesa = 7 + 1.5*personagem.atributo.constituicao;
-				}else if(escolha_armadura == 3){
-					strcpy(personagem.equipamento.armadura.nome, "armadura3");
-					personagem.equipamento.armadura.defesa = 10 + 1.5*personagem.atributo.constituicao;
-				} 
+			printf("\na hr de escolher a sua arma e comecar a sua jornada chegou...\n");
+			printf("vc tem tres opcoes de armas: \n1.katana : arma leve\n2.espada : arma leve\n3.machado : arma pesada\n\ndigite respectivamente 1,2 ou 3: ");
+			scanf("%i",&escolha_primeira_arma);
 
-				//printf("\n\n%s, %.1f\n\n", personagem.equipamento.armadura.nome, personagem.equipamento.armadura.defesa);
-			
-				break;
-				
-			case 2:
-				
-				break;
-				
-			case 3:
-				printf("Fechando...");
-				break;
-				
-			default:
-				printf("valor invalido,tente novamente.\n\n");
+			if(escolha_primeira_arma == 1)
+			{
+				strcpy(personagem.equipamento.arma.nome, "katana");
+				personagem.equipamento.arma.categoria = 1;
+				personagem.equipamento.arma = arma;
+			}
+			else if(escolha_primeira_arma == 2)
+			{
+				strcpy(personagem.equipamento.arma.nome, "espada");
+				personagem.equipamento.arma.categoria = 1;
+				personagem.equipamento.arma = arma;
+			}
+			else if(escolha_primeira_arma == 3)
+			{
+				strcpy(personagem.equipamento.arma.nome, "machado");
+				personagem.equipamento.arma.categoria = 2;
+				personagem.equipamento.arma = arma;
+			}
+			else
+			{
+				printf("resposta invalida,tente novamente.\n\n");
 				system("pause");
-				system("cls");	
+				system("cls");
+			}
+
+			printf("\nMomento de escolher a armadura\n");
+			printf("Escolha uma das 3 opcoes:\n1- armadura1\n2- armadura2\n3- armadura 3\n\ndigite respectivamente 1,2 ou 3: ");
+			scanf("%d", &escolha_armadura);
+
+			if(escolha_armadura == 1){
+				strcpy(personagem.equipamento.armadura.nome, "armadura1");
+				personagem.equipamento.armadura.defesa = 5 + 1.5*personagem.atributo.constituicao;
+			}else if(escolha_armadura == 2){
+				strcpy(personagem.equipamento.armadura.nome, "armadura2");
+				personagem.equipamento.armadura.defesa = 7 + 1.5*personagem.atributo.constituicao;
+			}else if(escolha_armadura == 3){
+				strcpy(personagem.equipamento.armadura.nome, "armadura3");
+				personagem.equipamento.armadura.defesa = 10 + 1.5*personagem.atributo.constituicao;
+			} 
+
+			//printf("\n\n%s, %.1f\n\n", personagem.equipamento.armadura.nome, personagem.equipamento.armadura.defesa);
+
+			break;
+
+		case 2:
+
+			break;
+
+		case 3:
+			printf("Fechando...");
+			break;
+
+		default:
+			printf("valor invalido,tente novamente.\n\n");
+			system("pause");
+			system("cls");	
 	}
 }
 
@@ -175,4 +173,3 @@ void teste(PERSONAGEM P)
 	printf("constituicao %i\n",P.atributo.constituicao);
 	printf("agilidade %i\n",P.atributo.agilidade);
 }
-
